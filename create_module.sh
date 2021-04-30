307d88fadf54901a5227570a61043342a545f812
Osama <crowdscoops@gmail.com>
2021-04-30 16:16:10 +0100

    devsecops_labs -> labtainer-labs

--- create_module.sh
+++ create_module.sh
@@ -16,14 +16,14 @@ git add ${1}
 git commit ${1} -m "Adding an IModule"
 cd $LABTAINER_DIR/distrib
 
-./publish.py -d -l ${1}
+./publish.py -d -f -l ${1}
 
 #cd /home/student/labtainer/trunk/scripts/designer/bin
 
 echo "=== `date` : Create an Imodule For Labtainer Lab $1 $==="
 # Create the imodule
 create-imodules.sh
-cp -rp ~/labtainer/trunk/labs/${1} ~/devsecops_labs/
+cp -rp ~/labtainer/trunk/labs/${1} ~/labtainer-labs/
 cp $LABTAINER_DIR/imodule.tar ~/labtainer-labs/imodules/${1}.tar
 
 echo "=== `date` : Remove the Git repository from labs$==="
@@ -32,9 +32,9 @@ cd ~/labtainer/trunk/labs/
 git update-ref -d HEAD
 git commit -m "reset to HEAD after creating $1 module"
 
-echo "=== `date` : Pushing the Lab and the Imodule to devsecops_lab Git Repository $1 $==="
-cd ~/devsecops_labs
-git add ~/devsecops_labs/imodules/${1}.tar
-git add ~/devsecops_labs/${1}
+echo "=== `date` : Pushing the Lab and the Imodule to labtainer-labs Git Repository $1 $==="
+cd ~/labtainer-labs
+git add ~/labtainer-labs/imodules/${1}.tar
+git add ~/labtainer-labs/${1}
 git commit -m "added/updated imodule $1"
 git push origin master
