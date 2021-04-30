070fe05f812e3dbf3d508696da9f492a039d4def
Osama <crowdscoops@gmail.com>
2021-04-30 16:33:06 +0100

    add utils to build, create and copy modules

---
+++ build_module.sh
@@ -0,0 +1,5 @@
+#   Build
+echo "=== `date` : Start Building Labtainer Lab $1 $==="
+
+cd ~/labtainer/labtainer-student
+rebuild -f -b ${1}
---
+++ copy_to_labs.sh
@@ -0,0 +1,2 @@
+#   Build
+rsync --delete -v -a ~/labtainer/trunk/labs/${1} ~/labtainer-labs/
